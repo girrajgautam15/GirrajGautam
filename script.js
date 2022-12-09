026f86ce519f6d2f03e726ad3e9b8e352baac5e1
Girraj <gautamgirraj86@gmail.com>
2022-12-09 13:43:11 +0530

    Projects

--- script.js
+++ script.js
@@ -1,54 +1,3 @@
-// $(document).ready(function(){
-//     $(window).scroll(function(){
-//         if(this.scrollY > 20){
-//             $('.nav-bar').addClass("sticky");
-//             console.log("Hi");
-//         }
-//         else{
-//             $('.nav-bar').removeClass("sticky");
-//         }
-
-//         if(this.scrollY > 500){
-//             $('.scroll-up-btn').addClass("show");
-//         }else{
-//             $('.scroll-up-btn').removeClass("show");
-//         }
-//     });
-
-//     $('.scroll-up-btn').click(function(){
-//         $('html').animate({scrollTop: 0});
-//         // removing smooth scroll on slide-up button click
-//         $('html').css("scrollBehavior", "auto");
-//     });
-//     $('.navbar .menu li a').click(function(){
-//         // applying again smooth scroll on menu items click
-//         $('html').css("scrollBehavior", "smooth");
-//     });
-
-
-
-//     // toggle menu/navbar script
-//     $('.menu-btn').click(function(){
-//         $('.nav-bar .menu').toggleClass("active");
-//         $('.menu-btn i').toggleClass("active");
-//     })
-
-//     var typed = new Typed(".typing", {
-//         strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
-//         typeSpeed: 100,
-//         backSpeed: 60,
-//         loop: true
-//     });
-//     var typed = new Typed(".typing-2", {
-//         strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
-//         typeSpeed: 100,
-//         backSpeed: 60,
-//         loop: true
-//     });
-
-
-// })
-
 $(document).ready(function () {
     $(window).scroll(function () {
         // sticky navbar on scroll script
@@ -90,14 +39,14 @@ $(document).ready(function () {
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Student","Web Developer", "UI/UX Designer","Travel Fanatic"],
+        strings: ["Student","Web Developer", "UI/UX Designer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Student","Web Developer", "UI/UX Designer","Travel Fanatic"],
+        strings: ["Student","Web Developer", "UI/UX Designer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
